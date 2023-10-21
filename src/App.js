d022cf168c9300c3ba5cbc9cdd0a03bdbbc78be2
azadov-azamat <azamatazadov112@gmail.com>
2023-10-21 13:10:37 +0500

    tailwindcss add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const links = [
+        {names: 'goole', link: 'https://www.google.com/'},
+        {names: 'yandex', link: 'https://ya.ru/'},
+        {names: 'instagram', link: 'https://www.instagram.com/'},
+        {names: 'vk', link: 'https://www.vk.com/'},
+        {names: 'twitter', link: 'https://twitter.com/'},
+        {names: 'twitter', link: 'https://twitter1.com/'}
+    ];
+
+    return (
+        <div className="App w-full ">
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+        </div>
+    );
 }
 
 export default App;
